9f0fc8e20be3c78f0c100827beb56fcf87ec1595
William Castro <wmcjimenez@gmail.com>
2023-04-20 17:04:43 -0500

    feat: :sparkles: Add components, pages & assets
    Menu, cesión de contrato, cambio de plan, traslados, cambio de SIM were added. Also, components like button, header & ordered list & assets like icons were included

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,35 +1,73 @@
 Page({
-  onLoad(query) {
-    // 页面加载
-    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
+  data: {
+    nodes: [
+      {
+        name: "span",
+        attrs: {
+          style: "color: #DB493E; font-weight: bold; font-size: 0.48rem;"
+        },
+        children: [
+          {
+            type: "text",
+            text: "Trámites"
+          }
+        ]
+      },
+      {
+        name: "span",
+        attrs: {
+          style: "color: #4e4e4e; font-weight: bold; font-size: 0.48rem;"
+        },
+        children: [
+          {
+            type: "text",
+            text: " para tu empresa"
+          }
+        ]
+      }
+    ],
+    proceduresMenu: [
+      {
+        id: "contract",
+        iconUrl: "/assets/svg/request-page.svg",
+        text: "Cesión de contrato"
+      },
+      {
+        id: "plan",
+        iconUrl: "/assets/svg/change-plan.svg",
+        text: "Cambio de plan"
+      },
+      {
+        id: "transfer",
+        iconUrl: "/assets/svg/move.svg",
+        text: "Traslados"
+      },
+      {
+        id: "sim",
+        iconUrl: "/assets/svg/sim-card.svg",
+        text: "Cambio de SIM card"
+      }
+    ]
   },
-  onReady() {
-    // 页面加载完成
-  },
-  onShow() {
-    // 页面显示
-  },
-  onHide() {
-    // 页面隐藏
-  },
-  onUnload() {
-    // 页面被关闭
-  },
-  onTitleClick() {
-    // 标题被点击
-  },
-  onPullDownRefresh() {
-    // 页面被下拉
-  },
-  onReachBottom() {
-    // 页面被拉到底部
+  goToPage(e) {
+    const navigates = {
+      contract: () =>
+        my.navigateTo({
+          url: "/pages/contract-assignment/contract-assignment"
+        }),
+      plan: () =>
+        my.navigateTo({ url: "/pages/plan-management/plan-management" }),
+      transfer: () => my.navigateTo({ url: "/pages/transfer/transfer" }),
+      sim: () => my.navigateTo({ url: "/pages/sim-management/sim-management" })
+    };
+    const pageName = e.id;
+    return navigates[pageName]();
   },
   onShareAppMessage() {
-    // 返回自定义分享信息
     return {
-      title: 'My App',
-      desc: 'My App description',
-      path: 'pages/index/index',
+      title: "My App",
+      desc: "My App description",
+      path: "pages/index/index"
     };
-  },
+  }
 });
