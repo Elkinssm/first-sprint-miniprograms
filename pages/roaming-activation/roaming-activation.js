2110594a8d3a2852e51f5b022bde541f73414bd5
Juan Diego Loboa Andrade <juan.loboa@neoris.com>
2023-05-02 13:34:19 -0500

    countries page

--- pages/roaming-activation/roaming-activation.js
+++ pages/roaming-activation/roaming-activation.js
@@ -2,9 +2,55 @@ Page({
   data: {
     cellphone: 3111111111,
     stepIndex: 1,
+    paises: ['Argentina', 'Chile', 'Colombia', 'México', 'Perú', 'Uruguay'],
+    pais1: '',
+    pais2: '',
+    pais3: '',
+    pais4: '',
+    pais5: '',
+    textNotFindCountry : 'Si no encuentras tu país destino favor comunícate al *611, O si estás por fuera de Colombia al + 57 320 999 9999'
+
   },
   handleButton(e) {
+    console.log(e);
     const { step } = e.target.dataset;
     this.setData({ stepIndex: Number(step) });
   },
+
+  onPickerChange: function(e) {
+    const index = e.currentTarget.id
+    const value = this.data.paises[e.detail.value];
+    const key = index;
+    console.log(key);
+    this.setData({
+      [key]: value
+    })
+    console.log(value)
+  },
+  selectOption: function (e) {
+    var value = e.currentTarget.dataset.value;
+    this.setData({
+      selectedOption: value
+    });
+  },
+continue: function(e){
+  const nextStep = this.data.stepIndex + 1;
+  e.target.dataset.step = nextStep;
+  console.log(  e.target.dataset.step)
+  if(this.data.pais1 === '' && this.data.pais2 === '' && this.data.pais3 === ''&& this.data.pais4 === ''&& this.data.pais5 === ''){
+    my.alert({
+      title : 'Debe seleccionar al menos 1 pais',
+      icon : 'none'
+    })
+  } 
+  else{
+    this.setData({
+      stepIndex : nextStep
+    })
+    this.handleButton(e)
+  }
+
+},
+
+
 });
