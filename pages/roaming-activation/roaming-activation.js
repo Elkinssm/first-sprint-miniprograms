89528ab8aa93e521d1582355a999a665313f72f4
William Castro <wmcjimenez@gmail.com>
2023-05-03 12:18:46 -0500

    feat: :zap: Add logic for the step 2
    Logic related to Paquetes in the 2nd step was added

--- pages/roaming-activation/roaming-activation.js
+++ pages/roaming-activation/roaming-activation.js
@@ -2,14 +2,35 @@ Page({
   data: {
     cellphone: 3111111111,
     stepIndex: 1,
-    paises: ['Argentina', 'Chile', 'Colombia', 'México', 'Perú', 'Uruguay'],
-    pais1: '',
-    pais2: '',
-    pais3: '',
-    pais4: '',
-    pais5: '',
-    textNotFindCountry : 'Si no encuentras tu país destino favor comunícate al *611, O si estás por fuera de Colombia al + 57 320 999 9999'
-
+    paises: ["Argentina", "Chile", "Colombia", "México", "Perú", "Uruguay"],
+    pais1: "",
+    pais2: "",
+    pais3: "",
+    pais4: "",
+    pais5: "",
+    textNotFindCountry:
+      "Si no encuentras tu país destino favor comunícate al *611, O si estás por fuera de Colombia al + 57 320 999 9999",
+    packageTypes: [
+      {
+        iconUrl: "/assets/icons/bundle.svg",
+        title: "Paquetes Bundle",
+        subtitle: "Paquetes de datos y voz",
+        packageType: "bundle",
+      },
+      {
+        iconUrl: "/assets/icons/bundle.svg",
+        title: "Paquetes de datos",
+        subtitle: "Paquetes de datos",
+        packageType: "datos",
+      },
+      {
+        iconUrl: "/assets/icons/bundle.svg",
+        title: "Paquetes voz",
+        subtitle: "Paquetes de voz",
+        packageType: "voz",
+      },
+    ],
+    selectedPackage: "",
   },
   handleButton(e) {
     console.log(e);
@@ -17,40 +38,48 @@ Page({
     this.setData({ stepIndex: Number(step) });
   },
 
-  onPickerChange: function(e) {
-    const index = e.currentTarget.id
+  onPickerChange: function (e) {
+    const index = e.currentTarget.id;
     const value = this.data.paises[e.detail.value];
     const key = index;
     console.log(key);
     this.setData({
-      [key]: value
-    })
-    console.log(value)
+      [key]: value,
+    });
+    console.log(value);
   },
   selectOption: function (e) {
     var value = e.currentTarget.dataset.value;
     this.setData({
-      selectedOption: value
+      selectedOption: value,
     });
   },
-continue: function(e){
-  const nextStep = this.data.stepIndex + 1;
-  e.target.dataset.step = nextStep;
-  console.log(  e.target.dataset.step)
-  if(this.data.pais1 === '' && this.data.pais2 === '' && this.data.pais3 === ''&& this.data.pais4 === ''&& this.data.pais5 === ''){
-    my.alert({
-      title : 'Debe seleccionar al menos 1 pais',
-      icon : 'none'
-    })
-  } 
-  else{
+  continue: function (e) {
+    const nextStep = this.data.stepIndex + 1;
+    e.target.dataset.step = nextStep;
+    console.log(e.target.dataset.step);
+    if (
+      this.data.pais1 === "" &&
+      this.data.pais2 === "" &&
+      this.data.pais3 === "" &&
+      this.data.pais4 === "" &&
+      this.data.pais5 === ""
+    ) {
+      my.alert({
+        title: "Debe seleccionar al menos 1 pais",
+        icon: "none",
+      });
+    } else {
+      this.setData({
+        stepIndex: nextStep,
+      });
+      this.handleButton(e);
+    }
+  },
+  handlePackageButton(e) {
+    const { packageType } = e.target.dataset;
     this.setData({
-      stepIndex : nextStep
-    })
-    this.handleButton(e)
-  }
-
-},
-
-
+      selectedPackage: packageType,
+    });
+  },
 });
