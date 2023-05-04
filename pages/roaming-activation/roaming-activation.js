64e3e871b9e5219b0bb6d4d07002118094a91b84
William Castro <wmcjimenez@gmail.com>
2023-05-04 10:25:14 -0500

    refactor: :art: Refactor countries logic

--- pages/roaming-activation/roaming-activation.js
+++ pages/roaming-activation/roaming-activation.js
@@ -3,11 +3,8 @@ Page({
     cellphone: 3111111111,
     stepIndex: 1,
     paises: ["Argentina", "Chile", "Colombia", "México", "Perú", "Uruguay"],
-    pais1: "",
-    pais2: "",
-    pais3: "",
-    pais4: "",
-    pais5: "",
+    selectedCountries: [],
+    numberOfAvailableCountries: 5,
     textNotFindCountry:
       "Si no encuentras tu país destino favor comunícate al *611, O si estás por fuera de Colombia al + 57 320 999 9999",
     packageTypes: [
@@ -39,32 +36,19 @@ Page({
   },
 
   onPickerChange: function (e) {
-    const index = e.currentTarget.id;
-    const value = this.data.paises[e.detail.value];
-    const key = index;
-    console.log(key);
+    const { value } = e.detail;
+    console.log(e);
+    const country = this.data.paises[value];
+    const addedCountry = this.data.selectedCountries.concat(country);
     this.setData({
-      [key]: value,
-    });
-    console.log(value);
-  },
-  selectOption: function (e) {
-    var value = e.currentTarget.dataset.value;
-    this.setData({
-      selectedOption: value,
+      selectedCountries: addedCountry,
     });
   },
   continue: function (e) {
     const nextStep = this.data.stepIndex + 1;
     e.target.dataset.step = nextStep;
     console.log(e.target.dataset.step);
-    if (
-      this.data.pais1 === "" &&
-      this.data.pais2 === "" &&
-      this.data.pais3 === "" &&
-      this.data.pais4 === "" &&
-      this.data.pais5 === ""
-    ) {
+    if (this.data.selectedCountries.length === 0) {
       my.alert({
         title: "Debe seleccionar al menos 1 pais",
         icon: "none",
