06b6e0a97bf5e1803207ca3238cd51494a7f870d
William Castro <wmcjimenez@gmail.com>
2023-05-05 18:09:58 -0500

    feat: :sparkles: Add suspension activation page

---
+++ pages/suspension-activation/suspension-activation.js
@@ -0,0 +1,70 @@
+import { totalLinksInternet } from "../../services/totalLinksInternet";
+import { totalLinksTelephone } from "../../services/totalLinksTelephone";
+import { listLinksTelephoneByAccount } from "../../services/listLinksTelephoneByAccount";
+import { listLinksInternetByAccount } from "../../services/listLinksInternetByAccount";
+
+Page({
+  data: {
+    enterpriseName: "No enterprise",
+    enterpriseNumber: "No number",
+    accountNumber: "127053",
+    numberLinksInternet: null,
+    numberLinksTelephone: null,
+  },
+  async handleInternetCollapseChange(e) {
+    const isOpened = e;
+    if (isOpened) {
+      try {
+        const response = await totalLinksInternet(this.data);
+        const data =
+          response.data.data.response.ns0totalLinksInternetResponse.return;
+        this.setData({ numberLinksInternet: data });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  async handleTelephoneCollapseChange(e) {
+    const isOpened = e;
+    if (isOpened) {
+      try {
+        const response = await totalLinksTelephone(this.data);
+        const data =
+          response.data.data.response.ns0totalLinksTelephoneResponse.return;
+        this.setData({ numberLinksTelephone: data });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  handleInputConfirm(e) {
+    console.log(e);
+  },
+  async handleInternetTapLink() {
+    try {
+      const response = await listLinksInternetByAccount(this.data);
+      const data = response.data.response;
+      this.setListLinksAndNavigate(data);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async handleTelephoneTapLink() {
+    try {
+      const response = await listLinksTelephoneByAccount(this.data);
+      const data = response.data.response;
+      this.setListLinksAndNavigate(data);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  setListLinksAndNavigate(data) {
+    my.setStorageSync({
+      key: "listLinks",
+      data: data,
+    });
+    my.navigateTo({
+      url: "/pages/suspension-activation/list-links/list-links",
+    });
+  },
+});
