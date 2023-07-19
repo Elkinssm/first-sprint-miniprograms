5786df114e473c4c67c1a9d2e267cbcee9dfd914
Alejandro <alejandro.patino@neoris.com>
2023-07-19 14:39:40 -0500

    design architecture for webview

--- pages/returnOfEquipment/returnOfEquipment.js
+++
@@ -1,7 +0,0 @@
-Page({
-  data: {
-    webViewUrl: "https://forms.office.com/Pages/ResponsePage.aspx?id=uCK7RixM_0CDYHtjNIISeUEuAggo7mFEp-_9Oh_RVb9UQjVIMkNLQVhaQjFDWE5TTFgzTFlOSFFHRS4u",
-  },
-  
-  onLoad() {},
-});
--- pages/soluciones-moviles/roaming-international/sessionIdConfig/sessionIdConfig.js
+++
@@ -1,9 +0,0 @@
-Page({
-  data: {},
-  onLoad() {},
-  configurarSessionId: function(e){
-  const sessionID= e.detail.value.sessionID;
-  getApp().globalData.sessionId=sessionID;
-  
-  }
-});
